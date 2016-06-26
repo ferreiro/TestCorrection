d43bafb60ff4cada3eb704cbf1f53c881eaf5cde
Jorge Ferreiro <me@jgferreiro.com>
2016-06-26 22:58:59 +0200

    Calculated Mark over 10

--- corregirTests.hs
+++ corregirTests.hs
@@ -4,13 +4,17 @@
 
 p1 = (Pregunta 2 5)
 p2 = (Pregunta 1 3)
-preguntas = [p1, p2]
+preguntas = [p1, p2, p1, p2, p1]
 
-m1 = (Modelo [1, 2])
-m2 = (Modelo [2, 1])
+m1 = (Modelo [1, 2, 1, 2, 1])
+m2 = (Modelo [2])
 modelos = [m1, m2]
 
-test = Test preguntas modelos
+test = (Test preguntas modelos)
+
+--respuestaEstudiante = (RespuestaEstudiante "414992032-W" 2 [(Respuesta 3)])
+respuestaEstudiante = (RespuestaEstudiante "414992032-W" 1 [(Respuesta 2), (Respuesta 1), (Respuesta 1), (Respuesta 0), (Respuesta 0)])
+correccion = (corrige test respuestaEstudiante)
 
 ---------------------------------------
 -- DATA STRUCTURES --------------------
@@ -61,50 +65,48 @@ data RespuestaEstudiante = RespuestaEstudiante {
     identificador :: String,
     modelo :: Int,
     respuestas :: [Respuesta]
-}
+} deriving (Show)
 
 data Respuesta = Respuesta {
     valor :: Int
-}
+}  deriving (Show)
 
 {--
-Como hemos podido observar en la función anterior, necesitamos uan lista de preguntas
-para poder comprobar si la lista de respuestas del usuario es correcta.
+En nuestro programa tenemos que poder corregir los tests de nuestros estudiantes.
+Para ello necesitamos tener una función que reciba el test y las respuestas dadas
+por el estudiante. Como cada estudiante puede responder un modelo de examen diferente.
 
-Por tanto, necesitamos obtener una lista de Preguntas para un modelo dado.
-Como el modelo es una lista de enteros que representan las permutaciones
-de preguntas, entonces tenemos que encontrar de devolver una lista de preguntas
-del testo pero con el orden que tiene el modelo.
+Entonces vamos a necesitar por un lado obtener una lista de preguntas que sigan
+el orden del modelo de examen para posteriormente utilizar nuestra función notaEstudiante
+que recibe una lista de preguntas (las del modelo del examen) y una lista de respuestas
+dadas por el usuario y devuelve la nota obtenida.
 
-Para esto, necesitamos 3 funciones:
-  1. pedirPreguntasDelModelo
-     Recibe un test, un modelo de dicho test y devuelve una lista de preguntas.
-     Para ello llama a cogerPreguntasDadoOrden y le pasa la lista de preguntas
-     que tiene el test y el orden de preguntas del modelo de test.
-
-  2. cogerOrdenPreguntas.
-      Devuelve una lista de enteros, cuyos elementos representan el la pregunta
-      a la que se refiere nuestro modelo.
-
-  3. cogerPreguntasDadoOrden.
-      Recibe una lista de preguntas y el orden de las preguntas del modelo.
-      Por tanto, lo que devuelve es una lista de preguntas (que sigue el orden de nuestro modelo)
+Con el resultado de dicha función, vamos a poder obtener la corrección del usuario:
+Tanto: identificador del alumno, puntuaciónTotal y puntuación sobre 10.
 
 --}
 
-pedirPreguntasDelModelo :: Test -> Modelo -> [Pregunta]
-pedirPreguntasDelModelo (Test preguntas modelos) modelo =
-   cogerPreguntasDadoOrden preguntas (cogerOrdenPreguntas modelo)
+data Correccion = Correccion {
+    identificadorAlumno :: String,
+    puntuacionTotal :: Float,
+    puntuacionSobre10 :: Float
+}  deriving (Show)
 
-cogerOrdenPreguntas :: Modelo -> [Int]
-cogerOrdenPreguntas (Modelo ordenPreguntas) = ordenPreguntas
+corrige :: Test -> RespuestaEstudiante -> Correccion
+corrige (Test preguntas modelos) (RespuestaEstudiante identificador indiceModelo respuestas) =
+        (Correccion
+                identificador
+                (notaEstudiante (cogerPreguntasDelModelo preguntas (modelos !! (indiceModelo-1))) respuestas)
+                (puntuacion10 (length preguntas) (notaEstudiante (cogerPreguntasDelModelo preguntas (modelos !! (indiceModelo-1))) respuestas))
+        )
 
--- va componiendo una lista de preguntas siguiente el orden dado
-cogerPreguntasDadoOrden :: [Pregunta] -> [Int] -> [Pregunta]
-cogerPreguntasDadoOrden [] _ = []
-cogerPreguntasDadoOrden _ [] = []
-cogerPreguntasDadoOrden (preguntas) (indice:restoIndices) =
-    (preguntas !! (indice - 1)) : cogerPreguntasDadoOrden(preguntas restoIndices)-- coger la pregunta
+-- Puntuación Sobre 10.
+-- El numero máximo de puntos posibles sería el numero de preguntas por 1 cada pregunta
+-- Regla de 3:  maxPuntosPosible ______ 10 nota maxima
+--              puntosObtenidos _______ X nota sobre 10
+puntuacion10 :: Int -> Float -> Float
+puntuacion10 numeroPreguntas notaObtenida =
+    (10 * notaObtenida) / fromIntegral numeroPreguntas
 
 {--
 Para calcular la nota de un estudiante vamos a necesitar dos funciones:
@@ -134,13 +136,56 @@ Para calcular la nota de un estudiante vamos a necesitar dos funciones:
 --}
 
 notaEstudiante :: [Pregunta] -> [Respuesta] -> Float
-notaEstudiante [] _ = 0
-notaEstudiante _ [] = 0
+notaEstudiante [] _ = 0.0
+notaEstudiante _ [] = 0.0
 notaEstudiante (pregunta:preguntas) (respuesta:respuestas) =
-    notaPregunta(pregunta respuesta) + notaEstudiante(preguntas respuestas)
+    (notaPregunta pregunta respuesta) + (notaEstudiante preguntas respuestas)
 
 notaPregunta :: Pregunta -> Respuesta -> Float
 notaPregunta (Pregunta respuestaCorrecta opciones) (Respuesta respuestaEstudiante)
-        | respuestaEstudiante == 0                  = 0 -- Respuesta en Blanco
-        | respuestaEstudiante == respuestaCorrecta  = 1 -- Respuesta correcta
-        | otherwise                                 = (-1) / (opciones - 1) -- Respuesta incorrecta, fórmula -1/(N-1) [Siendo N el número de alternativas]
+        | respuestaEstudiante == 0                  = 0.0 -- Respuesta en Blanco
+        | respuestaEstudiante == respuestaCorrecta  = 1.0 -- Respuesta correcta
+        | otherwise                                 = (-1.0) / (fromIntegral opciones - 1.0) -- Respuesta incorrecta, fórmula -1/(N-1) [Siendo N el número de alternativas]
+
+{--
+Como hemos podido observar en la función anterior, necesitamos uan lista de preguntas
+para poder comprobar si la lista de respuestas del usuario es correcta.
+
+Por tanto, necesitamos obtener una lista de Preguntas para un modelo dado.
+Como el modelo es una lista de enteros que representan las permutaciones
+de preguntas, entonces tenemos que encontrar de devolver una lista de preguntas
+del testo pero con el orden que tiene el modelo.
+
+Para esto, necesitamos 3 funciones:
+  1. cogerPreguntasDelModelo
+     Recibe un test, un modelo de dicho test y devuelve una lista de preguntas.
+     Para ello llama a cogerPreguntasDadoOrden y le pasa la lista de preguntas
+     que tiene el test y el orden de preguntas del modelo de test.
+
+  2. cogerOrdenPreguntas.
+      Devuelve una lista de enteros, cuyos elementos representan el la pregunta
+      a la que se refiere nuestro modelo.
+
+  3. cogerPreguntasDadoOrden.
+      Recibe una lista de preguntas y el orden de las preguntas del modelo.
+      Por tanto, lo que devuelve es una lista de preguntas (que sigue el orden de nuestro modelo)
+
+--}
+
+cogerPreguntasDelModelo :: [Pregunta] -> Modelo -> [Pregunta]
+cogerPreguntasDelModelo preguntasDelTest modelo =
+   cogerPreguntasDadoOrden preguntasDelTest (cogerOrdenPreguntas modelo)
+
+-- cogerPreguntasDelModelo :: Test -> Modelo -> [Pregunta]
+-- cogerPreguntasDelModelo (Test preguntas modelos) modelo =
+--    cogerPreguntasDadoOrden preguntas (cogerOrdenPreguntas modelo)
+
+cogerOrdenPreguntas :: Modelo -> [Int]
+cogerOrdenPreguntas (Modelo ordenPreguntas) = ordenPreguntas
+
+-- va componiendo una lista de preguntas siguiente el orden dado
+cogerPreguntasDadoOrden :: [Pregunta] -> [Int] -> [Pregunta]
+cogerPreguntasDadoOrden [] _ = []
+cogerPreguntasDadoOrden _ [] = []
+cogerPreguntasDadoOrden (preguntas) (indice:restoIndices) =
+    (preguntas !! (indice - 1)) : (cogerPreguntasDadoOrden preguntas restoIndices)-- coger la pregunta
